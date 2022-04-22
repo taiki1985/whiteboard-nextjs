9b138f99491e1288a34641313ae740f59d9b66f3
Taiki Nakashima <gw.nakashima.gw@gmail.com>
2022-04-22 18:17:59 +0900

    done layout Info Index

--- src/components/DataGrid.tsx
+++ src/components/DataGrid.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
+
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90 },
   {
@@ -34,6 +35,15 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -2,17 +2,37 @@ import MainFrame from '../../components/MainFrame'
 import type { NextPage } from 'next'
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-
+import DataGridInfo from '../../components/DataGrid';
+import Grid from '@mui/material/Grid';
+import Fab from '@mui/material/Fab';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
 import BasicSpeedDial from '../../components/SpeedDial';
+import CreateIcon from '@mui/icons-material/Create';
 import BasicBreadcrumbs from '../../components/Breadcrumbs';
 
 const Info: NextPage = () => {
   return (
       <MainFrame>
         <BasicBreadcrumbs />
-        <Typography variant='h2' gutterBottom>会社</Typography>
-        <BasicSpeedDial/>
+        <Grid container columns={{ xs:2, sm:3 }} spacing={2} sx={{ width: {xs: '100%', md: '65%'}}}>
+            <Grid item xs={1}>
+              <Typography variant='h3'>会社</Typography>
+            </Grid>
+            <Grid item xs={3} textAlign='right'>
+            <Fab variant="extended" color='primary'>
+              <CreateIcon  sx={{ mr: 1 }} />
+              新規作成
+            </Fab>
+            </Grid>
+            <Grid item xs={3}>
+              <DataGridInfo />
+            </Grid>
+            <Grid item xs={3} textAlign='right'>
+            <Fab color="secondary" aria-label="add">
+                <FirstPageIcon />
+              </Fab>
+            </Grid>
+          </Grid>
       </MainFrame>
   )
 }
